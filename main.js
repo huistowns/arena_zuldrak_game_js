9eba64ff2283f93d4ed3266f6d9fe28471989eaa
huistowns <huistowns@gmail.com>
2020-03-05 21:20:50 +0600

    много-чего

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-
+// Бургер меню
 $('.burger-menu').click( function() {
     $('.burger-menu-links').toggle("fast");
+});
+
+// Модальное окно hero-wiki
+
+let openModal = document.querySelector('.alliance-btn');
+let closeModal = document.querySelector("#close-modal");
+let modalWindowAlliance = document.querySelector('.modal-window__aliance');
+
+openModal.addEventListener('click', function() {
+    document.body.style.backgroundColor = "black";
+    modalWindowAlliance.style.display = "block";
+});
+
+closeModal.addEventListener('click', function() {
+    modalWindowAlliance.style.display = "none";
 });
